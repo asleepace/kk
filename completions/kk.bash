cc80bfd7804a2ad02bca386a718d7b88dba4701f
Colin <colin_teahan@yahoo.com>
2026-04-15 17:16:14 -0700

    Add get-secret, set-secret, list-secrets commands with bash completion

--- completions/kk.bash
+++ completions/kk.bash
@@ -1,6 +1,19 @@
 _kk_completions() {
-  local cur="${COMP_WORDS[COMP_CWORD]}"
-  local commands=$(ls -1 ~/.kk/commands)
-  COMPREPLY=($(compgen -W "$commands" -- "$cur"))
+  local commands
+
+  if [[ "${words[2]}" == "rename-screenshot" && "${#words[@]}" -eq 3 ]]; then
+    _files -g "*.png"
+  elif [[ "${words[2]}" == "get-secret" && "${CURRENT}" -eq 3 ]]; then
+    local secrets
+    secrets=($(security dump-keychain 2>/dev/null \
+      | awk -F'"' '/svce/ { svc=$4 } /acct/ && svc { print svc; svc="" }' \
+      | grep '^kk\.' \
+      | sed 's/^kk\.//' \
+      | sort))
+    _describe 'secret' secrets
+  else
+    commands=($(ls -1 ~/.kk/commands))
+    _describe 'command' commands
+  fi
 }
-complete -F _kk_completions kk
+compdef _kk_completions kk
